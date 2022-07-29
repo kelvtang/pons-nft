10bd76f9fb2f73361b724c0c22fb2787f1ab0af6
Uzair <47213777+Uzair05@users.noreply.github.com>
2022-07-29 12:17:21 +0800

    temp

--- FlowPolygonBridge/contracts/PonsNftMarket.sol
+++ FlowPolygonBridge/contracts/PonsNftMarket.sol
@@ -2,11 +2,35 @@
 pragma solidity ^0.8.0;
 
 contract PonsNftMarket {
-    function list() public payable returns () {}
+    struct listingCertificate {
+        address payable listerAddress;
+        string nftId;
+        uint256 listingCount;
+        uint256 nftSalesPrice; // denominator 10,000
+    }
+
+    mapping(string => listingCertificate) public listingCertificateList; // nftId ==> listingCertificate
+
+    function listForSale(
+        string memory nftId,
+        uint256 salesPricex100,
+        address listerAddress
+    ) public payable returns (listingCertificate memory) {
+        listingCertificate memory cert;
+
+        cert.listerAddress = payable(listerAddress);
+        cert.nftId = nftId;
+        cert.listingCount = 0;
+        cert.nftSalesPrice = salesPricex100;
+
+        listingCertificateList[nftId] = cert;
+
+        return listingCertificateList[nftId];
+    }
 
     function unlist() public {}
 
     function purchase() public {}
 
-    function mint() public {}
+    // function mint() public {}
 }
