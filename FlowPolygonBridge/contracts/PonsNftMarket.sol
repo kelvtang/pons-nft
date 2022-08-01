f5c2636037d1de1ddf8e1539f01756b8cd93c441
Uzair <47213777+Uzair05@users.noreply.github.com>
2022-08-01 18:03:53 +0800

    testout; contracts made; make polygon-side tunnel

--- FlowPolygonBridge/contracts/PonsNftMarket.sol
+++ FlowPolygonBridge/contracts/PonsNftMarket.sol
@@ -1,90 +1,95 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../libraries/stringUtils.sol";
+// import "../libraries/stringUtils.sol";
 import "../../contracts/FxERC721.sol";
 
-contract PonsNftMarket {
-    event nftPurchased(address from, address to, string nftId, uint256 amount);
-    event nftListed(address by, string nftId, uint256 amount);
-    event nftUnlisted(string nftId);
+// import "../../contracts/ERC721.sol";
+
+contract PonsNftMarket is FxERC721 {
+    event nftPurchased(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256 amount
+    );
+    event nftListed(address by, uint256 tokenId, uint256 amount);
+    event nftUnlisted(uint256 tokenId);
     error nftOfferTooLow();
     error nftNotFound();
 
     struct listingCertificate {
         address payable listerAddress;
-        string nftId;
+        uint256 tokenId;
         uint256 listingCount;
     }
 
-    // IterableMapping.itmap private listingCertificateCollection; // nftId ==> listingCertificate
+    // IterableMapping.itmap private listingCertificateCollection; // tokenId ==> listingCertificate
 
-    mapping(string => listingCertificate) private listingCertificateCollection;
+    mapping(uint256 => listingCertificate) private listingCertificateCollection;
 
-    mapping(string => uint256) private nftSalesPrice; // denominator 10,000 // nftId ==> listingPrice
-    string[] private nftForSale; // list of for sale. --> keys for nftSalesPrice
+    mapping(uint256 => uint256) private nftSalesPrice; // denominator 10,000 // tokenId ==> listingPrice
+    uint256[] private nftForSale; // list of for sale. --> keys for nftSalesPrice
 
-    function listForSale(string calldata nftId, uint256 salesPricex100)
+    function listForSale(uint256 tokenId, uint256 salesPricex100)
         public
         returns (listingCertificate memory)
     {
-        /*
-        TODO: be able to hold listed nfts.
-        TODO: nftId should be inferred from nft token
-        */
-
         listingCertificate memory cert;
 
         cert.listerAddress = payable(msg.sender);
-        cert.nftId = nftId;
+        cert.tokenId = tokenId;
         cert.listingCount = (
-            listingCertificateCollection[nftId].listingCount == 0
+            listingCertificateCollection[tokenId].listingCount == 0
                 ? 1
-                : listingCertificateCollection[nftId].listingCount + 1
+                : listingCertificateCollection[tokenId].listingCount + 1
         ); // reference at: https://stackoverflow.com/a/59463026
 
-        listingCertificateCollection[nftId] = cert;
+        listingCertificateCollection[tokenId] = cert;
 
-        nftSalesPrice[nftId] = salesPricex100;
-        nftForSale.push(nftId);
+        nftSalesPrice[tokenId] = salesPricex100;
+        nftForSale.push(tokenId);
 
-        emit nftListed(msg.sender, nftId, salesPricex100);
+        emit nftListed(msg.sender, tokenId, salesPricex100);
         return cert;
     }
 
-    function unlist(string calldata nftId) public {
+    function unlist(uint256 tokenId) public {
         uint256 end = nftForSale.length;
         for (uint256 i = 0; i < nftForSale.length; i++) {
-            if (
-                StringUtils.equal(nftForSale[i], nftId) /* keccak256(nftForSale[i]) == keccak256(nftId) */
-            ) {
-                nftForSale[i] = "";
-                nftSalesPrice[nftId] = 0;
+            if (nftForSale[i] == tokenId) {
+                nftForSale[i] = 0;
+                nftSalesPrice[tokenId] = 0;
                 delete end;
-                delete listingCertificateCollection[nftId];
+                delete listingCertificateCollection[tokenId];
 
-                emit nftUnlisted(nftId);
+                emit nftUnlisted(tokenId);
             }
         }
         revert nftNotFound();
     }
 
-    function purchase(string calldata nftId) external payable returns (bool) {
-        if (listingCertificateCollection[nftId].listingCount >= 1) {
-            if (msg.value < nftSalesPrice[nftId]) {
-                listingCertificateCollection[nftId].listerAddress.transfer(
+    function purchase(uint256 tokenId) external payable returns (bool) {
+        if (listingCertificateCollection[tokenId].listingCount >= 1) {
+            if (msg.value < nftSalesPrice[tokenId]) {
+                listingCertificateCollection[tokenId].listerAddress.transfer(
                     msg.value
                 );
-                /*
-                TODO: send held nft to msg.sender;
-                */
+
+                // Initiate transfer of nft from listed seller to new owner.
+                safeTransferFrom(
+                    _msgSender(),
+                    listingCertificateCollection[tokenId].listerAddress,
+                    tokenId
+                );
+
                 emit nftPurchased(
-                    listingCertificateCollection[nftId].listerAddress,
+                    listingCertificateCollection[tokenId].listerAddress,
                     msg.sender,
-                    nftId,
+                    tokenId,
                     msg.value
                 );
-                unlist(nftId);
+                unlist(tokenId);
             } else {
                 revert nftOfferTooLow();
             }
@@ -92,11 +97,11 @@ contract PonsNftMarket {
         revert nftNotFound();
     }
 
-    function getForSaleIds() public view returns (string[] memory) {
+    function getForSaleIds() public view returns (uint256[] memory) {
         return nftForSale;
     }
 
-    function getPrice(string calldata nftId) public view returns (uint256) {
-        return nftSalesPrice[nftId];
+    function getPrice(uint256 tokenId) public view returns (uint256) {
+        return nftSalesPrice[tokenId];
     }
 }
---
+++ FlowPolygonBridge/contracts/PonsTunnel.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// import "../libraries/stringUtils.sol";
+import "../../contracts/FxERC721.sol";
+import "../../contracts/ERC721.sol";
+
+contract PonsNftMarket is FxERC721 {
+    /*
+     TODO:
+     * Handle recieving nft from flow-polygon tunnel
+     * Handle sending nft through flow-polygon tunnel
+     * manage a holder account instead of burning nft --> hold&release mechanism.
+     */
+}
