76ba5d19f729e9657ddc065fcc402a8906cd69b4
Uzair <47213777+Uzair05@users.noreply.github.com>
2022-08-01 13:09:13 +0800

    pre eliminary market contract created

--- FlowPolygonBridge/contracts/PonsNftMarket.sol
+++ FlowPolygonBridge/contracts/PonsNftMarket.sol
@@ -1,36 +1,106 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "../libraries/stringUtils.sol";
+
+// import "../../contracts/FxERC721.sol";
+
+// import "../libraries/iterable_mapping.sol"; // --> create iterable
+
 contract PonsNftMarket {
+    /* 
+    get nft
+    emit data
+     */
+
+    event nftPurchased(address from, address to, string nftId, uint256 amount);
+    event nftListed(address by, string nftId, uint256 amount);
+    event nftUnlisted(string nftId);
+    error nftOfferTooLow();
+    error nftNotFound();
+
     struct listingCertificate {
         address payable listerAddress;
         string nftId;
         uint256 listingCount;
-        uint256 nftSalesPrice; // denominator 10,000
     }
 
-    mapping(string => listingCertificate) public listingCertificateList; // nftId ==> listingCertificate
+    // IterableMapping.itmap private listingCertificateCollection; // nftId ==> listingCertificate
 
-    function listForSale(
-        string memory nftId,
-        uint256 salesPricex100,
-        address listerAddress
-    ) public payable returns (listingCertificate memory) {
+    mapping(string => listingCertificate) private listingCertificateCollection;
+
+    mapping(string => uint256) private nftSalesPrice; // denominator 10,000 // nftId ==> listingPrice
+    string[] private nftForSale; // list of for sale. --> keys for nftSalesPrice
+
+    function listForSale(string calldata nftId, uint256 salesPricex100)
+        public
+        returns (listingCertificate memory)
+    {
+        /*
+        TODO: be able to hold listed nfts.
+        TODO: nftId should be inferred from nft token
+        */
+
         listingCertificate memory cert;
 
-        cert.listerAddress = payable(listerAddress);
+        cert.listerAddress = payable(msg.sender);
         cert.nftId = nftId;
-        cert.listingCount = 0;
-        cert.nftSalesPrice = salesPricex100;
+        cert.listingCount = (
+            listingCertificateCollection[nftId].listingCount == 0
+                ? 1
+                : listingCertificateCollection[nftId].listingCount + 1
+        ); // reference at: https://stackoverflow.com/a/59463026
 
-        listingCertificateList[nftId] = cert;
+        listingCertificateCollection[nftId] = cert;
 
-        return listingCertificateList[nftId];
+        nftSalesPrice[nftId] = salesPricex100;
+        nftForSale.push(nftId);
+
+        emit nftListed(msg.sender, nftId, salesPricex100);
+        return cert;
     }
 
-    function unlist() public {}
+    function unlist(string calldata nftId) public {
+        uint256 end = nftForSale.length;
+        for (uint256 i = 0; i < nftForSale.length; i++) {
+            if (
+                StringUtils.equal(nftForSale[i], nftId) /* keccak256(nftForSale[i]) == keccak256(nftId) */
+            ) {
+                nftForSale[i] = "";
+                nftSalesPrice[nftId] = 0;
+                delete end;
+                delete listingCertificateCollection[nftId];
 
-    function purchase() public {}
+                emit nftUnlisted(nftId);
+            }
+        }
+        revert nftNotFound();
+    }
 
-    // function mint() public {}
+    function purchase(string calldata nftId) external payable returns (bool) {
+        if (listingCertificateCollection[nftId].listingCount >= 1) {
+            if (msg.value < nftSalesPrice[nftId]) {
+                listingCertificateCollection[nftId].listerAddress.transfer(
+                    msg.value
+                );
+                /*
+                TODO: send held nft to msg.sender;
+                */
+                emit nftPurchased(
+                    listingCertificateCollection[nftId].listerAddress,
+                    msg.sender,
+                    nftId,
+                    msg.value
+                );
+                unlist(nftId);
+            } else {
+                revert nftOfferTooLow();
+            }
+        }
+        revert nftNotFound();
+    }
+
+    function getForSaleIds() public view returns (string[] memory) {
+        return nftForSale;
+    }
 }
