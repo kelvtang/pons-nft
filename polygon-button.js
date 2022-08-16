c86979e4b30012ae8c90b1ec853c73a8c9e186e5
Abdel004 <1155129200@link.cuhk.edu.hk>
2022-08-16 15:49:05 +0800

    edit flow of  button

--- polygon-button.js
+++ polygon-button.js
@@ -1,29 +1,19 @@
 import React, { useEffect, useState } from "react";
-import MetaMaskOnboarding from '@metamask/onboarding';
 import { ethers } from "ethers";
+import { Oval } from 'react-loader-spinner';
 import Web3 from "web3";
-import { flow_sdk_api } from "../../pons-nft/config.mjs";
-import fcl_api from '@onflow/fcl'
-import flow_types from '@onflow/types'
-import { send_transaction_ } from "../../pons-nft/utils/flow-api.mjs";
 
+const MARKETPLACE_ABI = []
+const MARKETPLACE_PROXY_ADDRESS = ""
 
-const Connect = () => {
-    const dappUrl = ""; // TODO enter your dapp URL. For example: https://uniswap.exchange. (don't enter the "https://")
-    const metamaskAppDeepLink = "https://metamask.app.link/dapp/" + dappUrl;
-    return (
-      <a href={metamaskAppDeepLink}>
-        <button>
-          Open in MetaMask
-        </button>
-      </a>
-    );
-  }
-  
-  function App() {
-  
-    const [shouldPoll, setShouldPoll] = useState(false)
-  
+function App() {
+
+    const [pollInfo, setpollInfo] = useState({
+      poll: false,
+      message: '',
+      error: '',
+      polled: false
+    })
   
     const poll = async ({ tokenId }) => {
   
@@ -56,8 +46,7 @@ const Connect = () => {
   
       }
   
-      // TODO: Edit hardcoded values
-      const marketplaceInstance = new ethers.Contract(MARKETPLACE_ADDRESS, MARKETPLACE_ABI, signer)
+      const marketplaceInstance = new ethers.Contract(MARKETPLACE_PROXY_ADDRESS, MARKETPLACE_ABI, signer)
   
       const executePoll = async (resolve, reject) => {
   
@@ -71,7 +60,7 @@ const Connect = () => {
             })
             .catch(_ => {
               // TODO: Based on actual path
-              fetch(`/revert/${tokenId}`, { method: 'GET' })
+              fetch(`http://localhost:3010/market/revert`, { method: 'POST', body: { tokenId: tokenId, marketPlaceInstance: marketplaceInstance } })
             })
         } else {
           setTimeout(executePoll, 1000, resolve, reject);
@@ -83,36 +72,65 @@ const Connect = () => {
   
     useEffect(() => {
   
-      if (shouldPoll) {
+      if (pollInfo.poll) {
         let params = (new URL(document.location)).searchParams;
         // TODO: Change depending on the name of the actual query
         const tokenId = params.get("tokenId");
   
-        await send_transaction_
-          (authorizer_(address)(key_id)(private_key)) // TODO: Edit hardcoded values
-          (authorizer_(address)(key_id)(private_key)) // TODO: Edit hardcoded values
-          ([authorizer_(address)(key_id)(private_key)]) // TODO: Edit hardcoded values
-          (`import PonsTunnelContract from 0xPONS
-            transaction(polygonRecepientAddress: String, nftSerialId: UInt64) {
-            prepare (ponsAccount : AuthAccount){
-               PonsTunnelContract .sendNftThroughTunnelUsingSerialId(nftSerialId: nftSerialId, ponsAccount : ponsAccount, ponsHolderAccount : ponsAccount, tunnelUserAccount : ponsAccount, polygonAddress: polygonRecepientAddress);
-            }`)
-          ([flow_sdk_api.arg(POLYGON_RECEPIENT_ADDRESS, flow_types.String), // TODO: Edit hardcoded values
-          flow_sdk_api.arg(tokenId, flow_types.UInt64)])
-  
-        poll({ tokenId: tokenId })
-          .then(async _ => {
-            setShouldPoll(_ => false)
-          });
+        // TODO: Based on actual path
+        fetch("http://localhost:3010/market/flowPurchase", { method: "POST", body: { tokenId: tokenId } })
+          .then(res => {
+            if (res.status === 202) {
+              poll({ tokenId: tokenId })
+                .then(async _ => {
+                  setpollInfo(prev => ({ ...prev, poll: false, message: "Nft purchased on polygon" }))
+                })
+                .catch(async _ => {
+                  setpollInfo(prev => ({ ...prev, poll: false, message: "", error: "Something went wrong while purchasing on polygon" }))
+                })
+            } else {
+              setpollInfo(prev => ({ ...prev, poll: false, message: "", error: "Something went wrong while purchasing on flow" }))
+            }
+          })
+          .catch(_ => {
+            setpollInfo(prev => ({ ...prev, poll: false, message: "", error: "Something went wrong while sending request" }))
+          })
       }
-    }, [shouldPoll])
+    }, [pollInfo])
   
-    return (
-      window.ethereum ?
-        <button onClick={() => setShouldPoll(_ => true)} disabled={shouldPoll}>Buy on Polygon</button> :
-        <Connect />
-    )
+    const isMobile = () => {
+      if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+        || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4))) {
+        return true
+      } else {
+        return false
+      }
+    }
   
+    if (window.ethereum && isMobile() && !pollInfo.polled) {
+      setpollInfo(prev => ({ ...prev, poll: true, message: "Purchasing on flow and transferring to polygon", polled: true }))
+    } else if (!window.ethereum && isMobile()) {
+      return (
+        <page>
+          <button onClick={() => window.location.assign('https://metamask.app.link/dapp/858a-61-244-192-118.ap.ngrok.io')} disabled={pollInfo.poll}>
+            Buy on Polygon
+          </button>
+        </page>
+      )
+    } else {
+      return (
+        pollInfo.poll ?
+          <page>
+            <Oval color="#5C5C5C" height={50} width={50} />
+            <hint _for="loading-text">{pollInfo.message}</hint>
+          </page>
+          :
+          <page>
+            // TODO: Redirect back to app here
+            <hint _for="redirection-text">{pollInfo.error ? pollInfo.error : pollInfo.message}</hint>
+          </page>
+      )
+    }  
   }
   
   export default App;
